945f967af2c72aa962101f772d3e6868dd5c8f59
AljonaS <165931579+AljonaS@users.noreply.github.com>
2024-04-03 19:21:35 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+
+
+    float temperature = +5;
+
+    if (temperature ==  "")  {
+        System.out.println("Wear warm jacket");
+  }
+    else if (season == "spring") {
+      System .out.println("Wear a t-shirt");
+    else {
+      System .out.println("Incorrect credentials");
+      }
+    }
+// winter, spring, summer, autumn
+ //warm jacket, t-shirt, swimming suite, rain coat 
+
+}  }
